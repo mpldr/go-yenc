cbd74db1850410ad48ff792c6cb4c80705f6cf57
Moritz Poldrack <ich@moritz-poldrac.de>
2021-02-15 23:48:56 +0100

    implemented bufio for all comparisons

--- testdata/benchmarks/data-throughput/main.go
+++ testdata/benchmarks/data-throughput/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 
 	yenc "git.sr.ht/~poldi1405/go-yenc/testdata/benchmarks"
 )
 
 func main() {
-	for i := 0; i < len(indata); i++ {
-		fmt.Print(yenc.YEnc(indata[i]))
+	var b byte
+	var e bool
+
+	reader := bufio.NewReader(bytes.NewReader(indata))
+	var eof bool
+	var err error
+
+	for !eof {
+		b, err = reader.ReadByte()
+		if err != nil {
+			eof = true
+		}
+
+		b, e = yenc.YEnc(b)
+		if e {
+			fmt.Print('=')
+		}
+		fmt.Print(b)
 	}
 }
